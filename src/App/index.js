b52f9ac8f504594c51a9a877565764f834404929
Lucas <lsalvatore2@gmail.com>
2018-10-30 12:23:33 -0400

    Create Welcome Message

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage'
 
 class App extends Component {
   render() {
     return (
-      <div> Hello World </div>
+      <WelcomeMessage/>
     );
   }
 }
