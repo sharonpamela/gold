46d356a8ea016d00ac64adbbfd13d80ab941828b
Lucas <lsalvatore2@gmail.com>
2018-10-31 08:04:22 -0400

    Stopping Point

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage'
-import styled, {css} from 'styled-components';
 
 class App extends Component {
   render() {
