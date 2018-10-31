1148b9fd69a29c62c6ea95d813551ac66d71c526
Lucas <lsalvatore2@gmail.com>
2018-10-31 11:52:08 -0400

    Finish Add/Remove Coins

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
 import CoinTile from "./CoinTile";
 
@@ -10,14 +10,14 @@ export const CoinGridStyled = styled.div`
   margin-top: 40px; 
 `
 
-function getCoinsToDisplay(coinList, topSection){
-  return Object.keys(coinList).slice(0, topSection ? 10 : 100);
+function getCoinsToDisplay(coinList, topSection, favorites){
+  return topSection ? favorites : Object.keys(coinList).slice(0, 100);
 }
 
 export default function ({topSection}){
   return <AppContext.Consumer>
-    {({coinList}) => <CoinGridStyled>
-      {getCoinsToDisplay(coinList, topSection).map(coinKey =>
+    {({coinList, favorites}) => <CoinGridStyled>
+      {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
         <CoinTile topSection={topSection} coinKey={coinKey}/>
       )}
     </CoinGridStyled>}
