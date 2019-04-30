f85ee7c1a1c885c09853608f59ee403f5e77cb1e
sharonpamela <sharon.pamela.santana@gmail.com>
2019-04-29 22:13:22 -0700

    completed google oath flow and updated keys

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    cookieKey: process.env.GOOGLE_CLIENT_COOKIE_KEY
+    cookieKey: process.env.GOOGLE_CLIENT_COOKIE_KEY,
+    mongoURI: process.env.MONGO_URI
 };
