776d8fb03ac1f746af80f9238d05e9e6285ff262
sharonpamela <sharon.pamela.santana@gmail.com>
2019-04-30 12:26:08 -0700

    debugging

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ router.use('/api', apiRoutes);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   // for production
-  if(process.env.NODE_ENV === 'production') {
+  // if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  } else {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  }
+  // } else {
+  //   res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  // }
 });
 
 module.exports = router;
